1cd48532ba4bca3caa59aa56bf1a0b03e9834109
Andy Fleener <anfleene@iupui.edu>
2010-08-26 12:47:32 -0400

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StopHoggingTheKeyboard::Application.config.secret_token = '83f90c4ce11653edad8a19336c836fef2a6a56bc229e6ee7c1e86c8dbfef525b6ce9842824fd65bfbf7077536cb51bdf9d33396a0c2fef8c9c1499c259cf9869'
