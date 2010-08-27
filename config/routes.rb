468464dc74bb48985c644708605c9e12d39df664
Andy Fleener <anfleene@iupui.edu>
2010-08-26 22:46:05 -0400

    ok so now i have a non rails default welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ StopHoggingTheKeyboard::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  match '/' => 'welcome#view'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -48,7 +47,7 @@ StopHoggingTheKeyboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "welcome#view"
 
   # See how all your routes lay out with "rake routes"
 
